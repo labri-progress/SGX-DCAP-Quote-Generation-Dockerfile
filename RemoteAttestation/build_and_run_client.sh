e06e7b3136f13858e2a4aa320e8098c3a8b8c6a3
Guilhem Niot <guilhem.niot@gmail.com>
2020-06-18 17:55:05 +0200

    Fix IO issue and use sockets instead (map localhost to host ip in client container)

--- RemoteAttestation/build_and_run_client.sh
+++ RemoteAttestation/build_and_run_client.sh
@@ -2,4 +2,4 @@ set -e
 docker build --target client --build-arg https_proxy=$https_proxy \
              --build-arg http_proxy=$http_proxy -t sgx_client -f ./Dockerfile .
 
-docker run --env http_proxy --env https_proxy --device=/dev/sgx/enclave -v /var/run/aesmd:/var/run/aesmd -it sgx_client
+docker run --net="host" --env http_proxy --env https_proxy --device=/dev/sgx/enclave -v ~/quote:/var/app/sgx-ra-sample/quote  -v /var/run/aesmd:/var/run/aesmd -it sgx_client 
--- RemoteAttestation/build_and_run_server.sh
+++ RemoteAttestation/build_and_run_server.sh
@@ -2,4 +2,4 @@ set -e
 docker build --target server --build-arg https_proxy=$https_proxy \
              --build-arg http_proxy=$http_proxy -t sgx_server -f ./Dockerfile .
 
-docker run --env http_proxy --env https_proxy --device=/dev/sgx/enclave -v /var/run/aesmd:/var/run/aesmd -it sgx_server
+docker run --env http_proxy --env https_proxy --device=/dev/sgx/enclave -v ~/quote:/var/app/sgx-ra-sample/quote -v /var/run/aesmd:/var/run/aesmd -p 7777:7777 -it sgx_server 
